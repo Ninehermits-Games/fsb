80c337cf4498993a5d11337e3bc23c2c5abb2541
Aaron <aaron@9hermits.com>
2024-11-18 12:12:02 +0530

    uhgf

--- Build/Build.framework.js
+++ Build/Build.framework.js
@@ -2141,34 +2141,34 @@ var ASM_CONSTS = {
     }
 
   async function _GetLeaderboard(leaderboard) {
-         if( window.unityInstance && _supabase  && window.Telegram.WebApp ){
-            try {
-              const view = UTF8ToString(leaderboard);
-              console.log({view});
-              const { data, error } = await window._supabase.from(view).select('*');
-              console.log({data});
-              // const arr = data.map(item => {
-              //   return {username:item.username, score:Number(item.score)};
-              // });
-              // console.log({leaderboard:arr});
-              // window.unityInstance.SendMessage(
-              //     "GameManager",
-              //     "OnLeaderboardReceived",
-              //    JSON.stringify(arr)
-              // );
-            } catch (getLeaderboardError){
-              console.log({getLeaderboardError});
-            }
-         }
+      try {
+        if (window.unityInstance && _supabase && window.Telegram.WebApp) {
+          const view = UTF8ToString(leaderboard);
+          console.log({ view });
+          const { data, error } = await window._supabase.from(view).select("*");
+          console.log({ data });
+          // const arr = data.map(item => {
+          //   return {username:item.username, score:Number(item.score)};
+          // });
+          // console.log({leaderboard:arr});
+          // window.unityInstance.SendMessage(
+          //     "GameManager",
+          //     "OnLeaderboardReceived",
+          //    JSON.stringify(arr)
+        }
+        // );
+      } catch (getLeaderboardError) {
+        console.log({ getLeaderboardError });
+      }
     }
 
   function _Initialization(url, key) {
-      if(window.unityInstance && window.Telegram.WebApp){
+      if (window.unityInstance && window.Telegram.WebApp) {
         const u = UTF8ToString(url);
         const k = UTF8ToString(key);
-        const _supabase = supabase.createClient(u,k);
+        const _supabase = supabase.createClient(u, k);
         window._supabase = _supabase;
-         console.log('Supabase Instance: ', _supabase);
+        console.log("Supabase Instance: ", _supabase);
       }
     }
 
@@ -4724,19 +4724,19 @@ var ASM_CONSTS = {
   	}
 
   async function _SendScore(score, game) {
-       if(window.unityInstance && _supabase && window.Telegram.WebApp){
+      if (window.unityInstance && _supabase && window.Telegram.WebApp) {
         try {
-          await window._supabase.from('scores').insert({
-            score: score, 
-            game:game, 
-            telegramid:window.Telegram.WebApp.initDataUnsafe.user.id, 
+          await window._supabase.from("scores").insert({
+            score: score,
+            game: game,
+            telegramid: window.Telegram.WebApp.initDataUnsafe.user.id,
             username: window.Telegram.WebApp.initDataUnsafe.user.username,
-            firstname: window.Telegram.WebApp.initDataUnsafe.user.first_name
+            firstname: window.Telegram.WebApp.initDataUnsafe.user.first_name,
           });
-        } catch (SendScoreError){
-          console.log({SendScoreError});
+        } catch (SendScoreError) {
+          console.log({ SendScoreError });
         }
-       }
+      }
     }
 
   function ___assert_fail(condition, filename, line, func) {
